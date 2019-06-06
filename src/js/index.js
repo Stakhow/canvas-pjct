312c17ee546ddaab330f66528f6f870991c64209
mac <mac@Mac-mini-mac.local>
2019-06-06 18:55:42 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+
+
+"use strict";
+$(document).ready(function(){
+
+  draw();
+
+}
+
+); // end document ready
+
+function draw() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 6; j++) {
+            ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ', ' +
+              Math.floor(255 - 42.5 * j) + ', 0)';
+            ctx.fillRect(j * 25, i * 25, 25, 25);
+        }
+    }
+}
